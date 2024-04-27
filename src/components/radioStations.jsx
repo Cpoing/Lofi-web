2a954d3f813fdc58f37b3ba3fec5dbcf116a19d2
Ted Lee <tedlee000@hotmail.com>
2024-04-27 15:01:21 -0400

    added lofi-girl to radiostation

--- src/components/radioStations.jsx
+++ src/components/radioStations.jsx
@@ -1,6 +1,6 @@
+import { motion } from "framer-motion";
 import React from "react";
 import { setActiveStation } from "../hooks/setActiveStation";
-import { motion } from "framer-motion";
 
 const RadioStations = ({ Ambient }) => {
   return (
@@ -26,6 +26,30 @@ const RadioStations = ({ Ambient }) => {
             alt=""
           />
           Ambient Renders
+        </motion.div>
+      </motion.div>
+
+      <motion.div animate={{ opacity: [0, 1] }} transition={{ delay: 0.85 }}>
+        <motion.div
+          whileHover={{ scale: 1.09 }}
+          whileTap={{ scale: 0.9 }}
+          onClick={Lofi}
+          className={
+            setActiveStation(StationName) === "LofiGirl"
+              ? "activeStation"
+              : "station"
+          }
+        >
+          <img
+            className={
+              setActiveStation(StationName) === "LofiGirl"
+                ? "activeTriangle"
+                : "triangle"
+            }
+            src={triangle}
+            alt=""
+          />
+          <span> Lofi Girl </span>
         </motion.div>
       </motion.div>
     </div>
